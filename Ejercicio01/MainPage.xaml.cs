20fd9f217c108561c74f9f21f83b13db3f96dbdf
amarosuarez <amaro.suarez@iesnervion.es>
2024-10-09 13:59:54 +0200

    Ejercicio 1 y 2 hecho

--- Ejercicio01/MainPage.xaml.cs
+++ Ejercicio01/MainPage.xaml.cs
@@ -6,6 +6,38 @@
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Al pulsar el boton 2 crea el boton 3
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnBt2Clicked(object sender, EventArgs e)
+        {
+            Button bt3 = new Button
+            {
+                Text = "Boton3",
+                BackgroundColor = Colors.Blue,
+                HeightRequest = 70,
+                WidthRequest = 200,
+                FontFamily = "Verdana",
+                FontSize = 16,
+                FontAttributes = FontAttributes.Bold,
+                BorderColor = Colors.Yellow,
+                BorderWidth = 4,
+                Margin = new Thickness(30)
+            };
+
+            (bt2.Parent as VerticalStackLayout)?.Children.Add(bt3);
+
+            bt3.Clicked += (s, args) =>
+            {
+                (bt1.Parent as VerticalStackLayout)?.Children.Remove(bt1);
+
+                bt2.FontSize = 8;
+                bt2.Text = "Crear controles en tiempo de ejecución mola";
+            };
+        }
     }
 
 }
---
+++ Ejercicio02/Page2.xaml.cs
@@ -0,0 +1,11 @@
+using Android.Media.Metrics;
+
+namespace Ejercicio02;
+
+public partial class Page2 : ContentPage
+{
+	public Page2()
+	{
+		InitializeComponent();
+	}
+}
